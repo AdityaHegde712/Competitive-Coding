df8bfdda765df122b6a7b091f6f0a672a24f1535
Aditya Hegde <adityahegde712@gmail.com>
2023-10-01 16:34:03 +0530

    Re-structured files

---
+++ Problem Solving/diagDiff.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    l_sum = 0
+    r_sum = 0
+    for i in range(3):
+        l_sum += arr[i][i]
+        r_sum += arr[i][2-i]
+    return abs(l_sum - r_sum)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
